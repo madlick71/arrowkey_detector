06c39e9befc3fa3cc4ebafe026f9526bb9933a46
Stefan Jösch <stefan_@mailbox.org>
2019-12-10 07:57:05 +0100

    update

--- pfeiltasten_trainer.py
+++ pfeiltasten_trainer.py
@@ -1,4 +1,3 @@
-from tkinter import Button, Label
 import tkinter as tk
 import random
 import time
@@ -18,33 +17,29 @@ class MyCoolApp:
         self.next_button = None
         self.clicks = 0
         self.clicks_correct = 0
-        self.sekunde = 0
-        self.minute = 0
-        # Buttons
-        self.button_left_arrow = Button(self.root, text=u'\u2BC7')
+        # tk.Buttons
+        self.button_left_arrow = tk.Button(self.root, text=u'\u2BC7')
         self.button_left_arrow.place(x=10, y=50, width=100, height=30)
-        self.button_right_arrow = Button(self.root, text=u'\u2BC8')
+        self.button_right_arrow = tk.Button(self.root, text=u'\u2BC8')
         self.button_right_arrow.place(x=120, y=50, width=100, height=30)
-        self.button_up_arrow = Button(self.root, text=u'\u2BC5')
+        self.button_up_arrow = tk.Button(self.root, text=u'\u2BC5')
         self.button_up_arrow.place(x=65, y=10, width=100, height=30)
-        self.button_down_arrow = Button(self.root, text=u'\u2BC6')
+        self.button_down_arrow = tk.Button(self.root, text=u'\u2BC6')
         self.button_down_arrow.place(x=65, y=90, width=100, height=30)
-        self.button_close = Button(self.root, text='Beenden', command=self.root.quit)
+        self.button_close = tk.Button(
+            self.root, text='Beenden', command=self.root.quit)
         self.button_close.place(x=270, y=250, width=100, height=30)
         # Textausgabe
-        self.label = Label(self.root, text="Los geht's!")
+        self.label = tk.Label(self.root, text="Los geht's!")
         self.label.config(font=('', 14))
         self.label.place(x=100, y=130, width=200, height=30)
         # Textausgabe Statistik
-        self.label_stat_1 = Label(self.root, text='')
+        self.label_stat_1 = tk.Label(self.root, text='')
         self.label_stat_1.config(font=('', 12))
         self.label_stat_1.place(x=100, y=180, width=200, height=20)
-        self.label_stat_2 = Label(self.root, text='')
+        self.label_stat_2 = tk.Label(self.root, text='')
         self.label_stat_2.config(font=('', 12))
         self.label_stat_2.place(x=100, y=200, width=200, height=20)
-        self.label_stat_uhr = Label(self.root, text='')
-        self.label_stat_uhr.config(font=('', 12))
-        self.label_stat_uhr.place(x=100, y=220, width=200, height=20)
         # auf Pfeiltasten hören
         self.root.bind("<Up>", self.up)
         self.root.bind("<Down>", self.down)
@@ -79,16 +74,16 @@ class MyCoolApp:
             button.config(background='#eeeeee')
         # nächste Taste zufällig auswählen und farblich markieren
         self.next_button = random.choice(buttons)
-        self.next_button.after(50, lambda:  self.next_button.config(background=self.farbe_vorgabe))
+        self.next_button.after(50, lambda:  self.next_button.config(
+            background=self.farbe_vorgabe))
 
     def press_button(self, button):
         self.button_pressed = button
-        # Uhr starten
-        # self.uhr()
         self.clicks = self.clicks + 1
         # gedrückte Taste simulieren
         self.button_pressed.config(relief=tk.SUNKEN)
-        self.button_pressed.after(100, lambda:  self.button_pressed.config(relief=tk.RAISED))
+        self.button_pressed.after(
+            100, lambda:  self.button_pressed.config(relief=tk.RAISED))
         # prüfen, welche Taste gedrückt wurde
         ok = self.check_which_pressed()
         # nächste Taste vorgeben
@@ -100,7 +95,8 @@ class MyCoolApp:
             self.clicks_correct = self.clicks_correct + 1
             wrong = self.clicks - self.clicks_correct
             self.label.configure(text='yeah!  :D')
-            self.label_stat_1.configure(text='Richtig: ' + str(self.clicks_correct))
+            self.label_stat_1.configure(
+                text='Richtig: ' + str(self.clicks_correct))
             self.label_stat_2.configure(text='Falsch: ' + str(wrong))
             self.button_pressed.config(background=self.farbe_richtig)
             self.button_pressed.after(
@@ -108,24 +104,14 @@ class MyCoolApp:
             return True
         else:
             self.label.configure(text='nope!  :(')
-            self.label_stat_1.configure(text='Richtig: ' + str(self.clicks_correct))
+            self.label_stat_1.configure(
+                text='Richtig: ' + str(self.clicks_correct))
             wrong = self.clicks - self.clicks_correct
             self.label_stat_2.configure(text='Falsch: ' + str(wrong))
             self.button_pressed.config(background=self.farbe_falsch)
             self.button_pressed.flash()
             self.button_pressed.config(background='#eeeeee')
             return False
-
-    def uhr(self):
-        while True:
-            while self.sekunde <= 59 and self.minute <= 59:
-                self.label_stat_uhr.configure(
-                    text='Zeit: ' + str(self.minute) + ':' + str(self.sekunde))
-                time.sleep(1)
-                self.sekunde = self.sekunde + 1
-                if self.sekunde == 59:
-                    self.sekunde = 0
-                    self.minute = self.minute + 1
 
 
 app = MyCoolApp()
